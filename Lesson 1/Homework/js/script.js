060484c567cb396f0a74a88a68babf7c58f5e56a
Edward3635 <Edgard3635@gmail.com>
2022-05-30 14:06:53 +0300

    Lesson 1

--- Lesson 1/Homework/js/script.js
+++ Lesson 1/Homework/js/script.js
@@ -19,23 +19,23 @@ document.write('<h1>x = 6, y = 14, z = 4;</h1>');
 
 document.write('<h4>1) x += y - x++ * z</h4>');
 document.write('<h5>x = ' + res1 + '</h5>');
-document.write('<h6>x * z -> x++ -> y - (x * z) -> x+(y - (x * z)</h6>');
+document.write('<h6> x++ -> x * z -> y - (x * z) -> x + ( y - ( x * z )</h6>');
 
 document.write('<h4>2) z = --x - y * 5;</h4>');
 document.write('<h5>z = ' + res2 + '</h5>');
-document.write('<h6>--x -> y * 5 ->--x - (y * 5)</h6>');
+document.write('<h6>--x -> y * 5 -> x - ( y * 5 )</h6>');
 
 document.write('<h4>3) y /= x + 5 % z;</h4>');
 document.write('<h5>y = ' + res3 + '</h5>');
-document.write('<h6>5 % z -> x + (5 % z) -> y/(x + (5 % z ))</h6>');
+document.write('<h6>5 % z -> x + ( 5 % z ) -> y / ( x + ( 5 % z ) )</h6>');
 
 document.write('<h4>4) z - x++ + y * 5;</h4>');
 document.write('<h5>result = ' + res4 + '</h5>');
-document.write('<h6>y * 5 -> z - x -> x++ -> (z - x) + (y * 5)</h6>');
+document.write('<h6> x++ -> y * 5 -> z - x -> ( z - x ) + ( y * 5 )</h6>');
 
 document.write('<h4>5) x = y - x++ * z;</h4>');
 document.write('<h5>x = ' + res5 + '</h5>');
-document.write('<h6>x * z -> x++ ->y - (x*z)</h6>');
+document.write('<h6> x++ -> x * z -> y - ( x * z )</h6>');
 
 document.write('</div>');
 document.write('</div>');
