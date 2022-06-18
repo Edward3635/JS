fbc64652da2236f5284dbe728d78c668de8b8a03
Edward3635 <Edgard3635@gmail.com>
2022-06-18 20:01:06 +0300

    Lesson 6

--- Lesson 6/js/script.js
+++ Lesson 6/js/script.js
@@ -1,27 +1,81 @@
 // Classwork
 
+function Calculator(x = 2, y = 15) {
+	this.x = x;
+	this.y = y;
+}
+Calculator.prototype.read = function () {
+	this.x = Number(prompt('Перше значення: ', this.x));
+	this.y = Number(prompt('Друге значення: ', this.y));
+};
+Calculator.prototype.sum = function (x, y) {
+	return this.x + this.y;
+};
+Calculator.prototype.mul = function (x, y) {
+	return this.x * this.y;
+};
+const calc = new Calculator();
+calc.read();
+console.log(calc.sum());
+console.log(calc.mul());
+
 // Homework 1.1
-// Constructor
-function Human(name, gender, age) {
+// Функція-конструктор
+function Human(name = 'Jennifer', gender = 'female', age = 45) {
+	// Це все властивості рівня екземпляр, вони дублюються в кожному екземплярі конструктора Human
 	this.name = name;
 	this.gender = gender;
 	this.age = age;
 }
-// Створення об'єктів через конструктор
-let human1 = new Human('Josh', 'man', 13), human2 = new Human('Steve', 'man', 19), human3 = new Human('Margot', 'woman', 41),
-	human4 = new Human('Barry', 'man', 25), human5 = new Human('Monika', 'woman', 22), human6 = new Human('Johny', 'man', 8),
-	human7 = new Human('Stella', 'woman', 81), human8 = new Human('Amelia', 'woman', 16),
-	human9 = new Human('Isabella', 'woman', 55);
 
-// Array
-const humanArr = [
-	human1, human2, human3, human4, human5, human6, human7, human8, human9];
-console.log(humanArr);
+// Масив екземплярів
+const humanArr = [new Human('Josh', 'male', 13), new Human('Steve', 'male', 19), new Human('Margot', 'female', 41),
+new Human('Barry', 'male', 25), new Human('Monika', 'female', 22), new Human('Johny', 'male', 8),
+new Human('Stella', 'female', 25), new Human('Amelia', 'female', 16), new Human('Isabella', 'female', 55)];
 
+console.log("Масив об'єктів, до сортування:", humanArr);
+
+console.log("Масив об'єктів, після сортування значень Age в порядку зростання:", sortByAge(humanArr));
+
+// Функція сортування значень Age в порядку зростання
 function sortByAge(arr) {
-	arr.sort((a, b) => a.age > b.age ? 1 : -1);
-	console.log(arr);
+	let copyArr = arr.slice();
+	copyArr.sort((a, b) => a.age > b.age ? 1 : -1);
+	return copyArr;
 }
-sortByAge(humanArr);
 
 // Homework 1.2
+// Функція-конструктор, що створює об'єкт Human вже існує
+// Методи рівня екземпляру будуть дублюватися в усіх об'єктах, створених через функцію і займатимуть багато пам'яті.
+// Тому усі методи виносяться за межі конструктора і потім викликається той чи інший метод
+// для ініціатору виклику - екземпляру, який в цьому потребує.
+// Властивості зазвичай пишуться в на рівні екземпляру, але вони також можуть писатися у прототипі,
+// якщо властивість додається лише певним об'єктам.
+// Властивості та методи функції-конструктора являються статичними і вони не наслідуються.
+
+
+Human.hobby = 'Бокс'; // Властивість - рівень конструктора, але цю властивість не побачать екземпляри
+
+// Метод - рівня конструктор
+Human.createHuman = function (name, gender, age) {
+	return new Human(name, gender, age);
+};
+
+// Метод прототипу
+Human.prototype.hello = function () {
+	let hello = `Привіт, моє ім'я - ${this.name}, мені ${this.age} років, давай знайомитись! Як тебе звати?`;
+	return hello;
+};
+Human.prototype.hobby = 'Хокей';
+
+console.log('');
+console.log(`${humanArr[7].name} обожнює ${humanArr[7].hobby}! А Бокс, на жаль, ні!`);
+console.log('');
+// Ініціалізація виклику методу hello() із прототипу екземпляром класу Human
+console.log(humanArr[4].hello()); //Привіт, моє ім'я - Josh, мені 13 років, давай знайомитись! Як тебе звати?
+console.log('');
+// Виклик методу рівня конструктор , в якому викликається метод hello() із прототипу.
+// Аргументів не задано, отже будуть задані конструктором
+console.log(Human.createHuman().hello()); // Привіт, моє ім'я - Jennifer, мені 45 років, давай знайомитись! Як тебе звати?
+console.log('');
+
