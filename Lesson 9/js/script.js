02db43d8edfa8ecbb602fa9135eef0e8c5b7dcea
Edward3635 <Edgard3635@gmail.com>
2022-07-01 11:11:12 +0300

    Lesson 9

---
+++ Lesson 9/js/script.js
@@ -0,0 +1,169 @@
+// Classwork секундомер
+
+let msec = 0, sec = 0, min = 0, hour = 0, intervalHandler, isStart = true;
+const getSelector = selector => document.querySelector(selector),
+	buttons = getSelector('.buttons'),
+	display = getSelector('.display__container'),
+	count = () => {
+		let content;
+		switch (true) {
+			case (hour < 10):
+				switch (true) {
+					case (min < 10):
+						switch (true) {
+							case (sec < 10):
+								content = `0${hour}:0${min}:0${sec}`; break;
+							default:
+								content = `0${hour}:0${min}:${sec}`; break;
+						} break;
+					case (min >= 10):
+						switch (true) {
+							case (sec < 10):
+								content = `0${hour}:${min}:0${sec}`; break;
+							default:
+								content = `0${hour}:${min}:${sec}`; break;
+						}break;
+				}break;
+			case (hour >= 10):
+				switch (true) {
+					case (min < 10):
+						switch (true) {
+							case (sec < 10):
+								content = `${hour}:0${min}:0${sec}`; break;
+							default:
+								content = `${hour}:0${min}:${sec}`; break;
+						}
+						break;
+					case (min >= 10):
+						switch (true) {
+							case (sec < 10):
+								content = `${hour}:${min}:0${sec}`; break;
+							default:
+								content = `${hour}:${min}:${sec}`; break;
+						}break;
+				}break;
+		}
+		getSelector('.display__text').innerHTML = content;
+
+		if (msec === 99) {
+			msec = 0, sec++;
+		}
+		if (sec === 60) {
+			sec = 0, min++;
+		}
+		if (min === 60) {
+			min = 0, hour++;
+		}
+		msec++;
+	};
+buttons.addEventListener('click', e => {
+	if (e.target.classList.contains('start')) {
+		if (isStart) {
+			isStart = false;
+			display.style.backgroundColor = 'rgb(2, 61, 2)';
+			intervalHandler = setInterval(count, 10);
+		}
+	}
+	if (e.target.classList.contains('stop')) {
+		clearInterval(intervalHandler);
+		isStart = true;
+		display.style.backgroundColor = 'red';
+	}
+	if (e.target.classList.contains('reset')) {
+		clearInterval(intervalHandler);
+		isStart = true;
+		msec = 0, sec = 0, min = 0;
+		display.style.backgroundColor = 'gray';
+		getSelector('.display__text').innerHTML = '00:00:00';
+	}
+});
+
+
+// Homework Вгадай значення
+
+let randomNumber = Math.floor(Math.random() * 100) + 1,
+	btn__guessNumber = getSelector('.btn__guess-number');
+btn__guessNumber.onclick = showRandomNumber;
+function showRandomNumber() {
+	const numberInputValue = getSelector('.input__number').value;
+	// Перевірка вводу регулярним виразом
+	if (numberInputValue.replace(/\D/, '')) {
+		if (numberInputValue > randomNumber) {
+			getSelector('.input__title').style.color = 'red';
+			getSelector('.input__title').textContent = 'Загадане число менше введеного Вами.';
+			getSelector('.input__number').value = '';
+		} else if (numberInputValue < randomNumber) {
+			getSelector('.input__title').style.color = 'red';
+			getSelector('.input__title').textContent = 'Загадане число більше введеного Вами.';
+			getSelector('.input__number').value = '';
+		} else {
+			getSelector('.input__title').style.color = 'green';
+			getSelector('.input__title').textContent = `Вітаю! Загадане число - ${randomNumber}.
+			Граєм далі, яке наступне???`;
+			randomNumber = Math.floor(Math.random() * 100) + 1;
+
+
+		}
+
+	} else {
+		getSelector('.input__title').textContent = 'Треба ввести число! (Цифрами)';
+	}
+
+}
+
+// Homework авторизація
+
+const btn__signIn = getSelector('.sign__in');
+btn__signIn.onclick = authorization;
+function authorization() {
+	const inputLoginValue = getSelector('.login').value, inputPasswordValue = getSelector('.password').value,
+		titleLogin = getSelector('.auth__title-login'), titlePassword = getSelector('.auth__title-password'),
+		titleSignIn = getSelector('.auth__title-signIn');
+	function resetStyle(arg) {
+		titlePassword.textContent = '';
+		titleLogin.textContent = '';
+		titleSignIn.textContent = '';
+		if (arg === 'login') {
+			titleLogin.textContent = 'Ви не заповнили поле login';
+		} else if (arg === 'pass') {
+			titlePassword.textContent = 'Ви не заповнили поле password';
+		} else if (arg === 'logpass') {
+			titleLogin.textContent = 'Ви не заповнили поле login';
+			titlePassword.textContent = 'Ви не заповнили поле password';
+		} else {
+			titleSignIn.textContent = 'Логін або пароль не вірні';
+		}
+	}
+	function hideText() {
+		titleSignIn.textContent = '';
+	}
+
+	if (inputLoginValue !== '') {
+		if (inputPasswordValue === '') {
+			resetStyle('pass');
+		} else {
+			if (inputLoginValue === 'admin') {
+				if (inputPasswordValue === '12345') {
+					titleSignIn.style.color = 'green';
+					titleSignIn.textContent = 'Ви авторизовані у системі!';
+					setTimeout(hideText, 2500);
+				} else {
+					resetStyle();
+
+				}
+			} else {
+				resetStyle();
+			}
+		}
+	} else {
+		if (inputPasswordValue === '') {
+			resetStyle('logpass');
+
+		} else {
+			resetStyle('login');
+		}
+
+
+
+	}
+}
