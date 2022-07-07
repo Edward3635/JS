1cbf57dc4d54163a14e180099c2972fcfb66dcfb
Edward3635 <Edgard3635@gmail.com>
2022-07-07 10:08:46 +0300

    Lesson 9

--- Lesson 9/js/script.js
+++ Lesson 9/js/script.js
@@ -1,14 +1,16 @@
-// Classwork секундомер
-
+// Секундомір
+// Ініціалізація змінних
 let msec = 0, sec = 0, min = 0, hour = 0, intervalHandler, isStart = true;
+/// Користувацька функція для отримання елементу сторінки по id/class/tag.
 const getSelector = selector => document.querySelector(selector),
 	buttons = getSelector('.buttons'),
 	display = getSelector('.display__container'),
 	inputNumber = getSelector('.input__number'),
 	count = () => {
+		// Автоматично додається нуль перед однозначним числом (1-9), по типу 01,02,03... За допомогою padStart()
 		getSelector('.display__text').innerHTML =
 			`${hour.toString().padStart(2, '0')}:${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}`;
-
+		// Логіка секундоміру: рахування мілісекунд - секунд -хвилини - годин
 		if (msec === 99) {
 			msec = 0, sec++;
 			if (sec === 60) {
@@ -20,6 +22,8 @@ const getSelector = selector => document.querySelector(selector),
 		}
 		msec++;
 	};
+// Панель керування
+// Кнопки старт, стоп і ресет.
 buttons.addEventListener('click', e => {
 	if (e.target.classList.contains('start')) {
 		if (isStart) {
@@ -43,23 +47,24 @@ buttons.addEventListener('click', e => {
 });
 
 
-// Homework Вгадай значення
+// Додаткове завдання: гра "Вгадай значення" від 1 до 100
 // Для зручності, можна через Enter вводити значення
 inputNumber.addEventListener('keyup', (e) => {
 	if (e.code === 'Enter') {
-		//if (getSelector('.input__number') === document.activeElement) {
 		showRandomNumber();
-		//}
+
 	}
 
 });
-
+// Генерація випадкового числа
 let randomNumber = Math.floor(Math.random() * 100) + 1,
 	btn__guessNumber = getSelector('.btn__guess-number');
+// Слідкування за кліком
 btn__guessNumber.onclick = showRandomNumber;
 function showRandomNumber() {
+	// Отримання значення із поля інпут
 	const numberInputValue = inputNumber.value;
-	// Перевірка вводу регулярним виразом
+	// Валідація
 	if (numberInputValue.replace(/\D/, '')) {
 		if (numberInputValue > randomNumber) {
 			getSelector('.input__title').style.color = 'red';
@@ -74,6 +79,8 @@ function showRandomNumber() {
 			getSelector('.input__title').textContent = `Вітаю! Загадане число - ${randomNumber}.
 			Граєм далі, яке наступне???`;
 			inputNumber.value = '';
+			// Після вгадування числа привітаємо гравця і гра починається знову автоматично
+			// Генерується нове число
 			randomNumber = Math.floor(Math.random() * 100) + 1;
 
 
@@ -86,14 +93,17 @@ function showRandomNumber() {
 
 }
 
-// Homework авторизація
+// Альтернатива третьому завданню класної роботи: сторінка авторизації, логін admin пароль 12345
 
 const btn__signIn = getSelector('.sign__in');
+// Слідкування за подією
 btn__signIn.onclick = authorization;
 function authorization() {
+	// Ініціалізація необхідних змінних і даних
 	const inputLoginValue = getSelector('.login').value, inputPasswordValue = getSelector('.password').value,
 		titleLogin = getSelector('.auth__title-login'), titlePassword = getSelector('.auth__title-password'),
 		titleSignIn = getSelector('.auth__title-signIn');
+	// Функція, що виводить відповідь на спроби авторизуватися
 	function resetStyle(arg) {
 		titlePassword.textContent = '';
 		titleLogin.textContent = '';
@@ -112,8 +122,8 @@ function authorization() {
 			titleSignIn.textContent = 'Логін або пароль не вірні';
 		}
 	}
+	// Функція, що ховатиме відповідь на спроби авторизуватися
 	function hideText(arg) {
-		//titleSignIn.textContent = '';
 		if (arg === 'login') {
 			titleLogin.textContent = '';
 		} else if (arg === 'pass') {
@@ -127,7 +137,7 @@ function authorization() {
 			titleSignIn.textContent = '';
 		}
 	}
-
+	// Перевірка авторизації
 	if (inputLoginValue !== '') {
 		if (inputPasswordValue === '') {
 			resetStyle('pass');
@@ -137,7 +147,8 @@ function authorization() {
 				if (inputPasswordValue === '12345') {
 					resetStyle('correct');
 					setTimeout(hideText, 1500, 'correct');
-					setTimeout(() => document.location = 'http://www.mozilla.org', 2200);
+					setTimeout(() => document.location =
+						'https://risovach.ru/upload/2013/03/mem/toni-stark_13447470_big_.jpeg', 2200);
 				} else {
 					resetStyle();
 					setTimeout(hideText, 3500);
@@ -163,12 +174,14 @@ function authorization() {
 	}
 }
 
-// Slider
+// Простенький слайдер
+// Через setInterval кожні 3 секунди змінюється картинка за допомогою цикла
 let sliderCounter = 2;
 setInterval(() => {
 	const img = document.querySelector('.slider-img');
 	img.src = `./img/img${sliderCounter}.jpg`;
 	sliderCounter++;
+	// Обнулити каунтер, коли дойде до останньої картинки
 	if (sliderCounter > 5) {
 		sliderCounter = 1;
 	}
