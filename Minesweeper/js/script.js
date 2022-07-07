c46e9e35587d8584dc20ab6d26eec4c7e9c6f133
Edward3635 <Edgard3635@gmail.com>
2022-07-07 21:14:21 +0300

    minesweeper

--- Minesweeper/js/script.js
+++ Minesweeper/js/script.js
@@ -1,3 +1,4 @@
 // Оголошення змінних та стрілкових функцій.
 /// Користувацька функція для отримання елементу сторінки по id/class/tag.
 const getSelector = arg => document.querySelector(arg);
+let counter;
