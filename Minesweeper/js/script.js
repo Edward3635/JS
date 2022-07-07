ed0a939aad88e841f5acf1bbf8c6884005dd4c5e
Edward3635 <Edgard3635@gmail.com>
2022-07-07 23:34:54 +0300

    minesweeper

--- Minesweeper/js/script.js
+++ Minesweeper/js/script.js
@@ -1,4 +1,11 @@
 // Оголошення змінних та стрілкових функцій.
 /// Користувацька функція для отримання елементу сторінки по id/class/tag.
 const getSelector = arg => document.querySelector(arg);
-let counter;
+
+startGame(8, 8, 10);
+
+function startGame(width, height, bombsNumber) {
+	const field = getSelector('.grid__field'), cellsCount = width * height;
+	field.innerHTML = '<button></button>'.repeat(cellsCount);
+
+}
