42848f586c96ddaae5031d9154e983f2026a37b2
Edward3635 <Edgard3635@gmail.com>
2022-07-10 00:41:01 +0300

    Minesweeper

--- Minesweeper/js/script.js
+++ Minesweeper/js/script.js
@@ -1,13 +1,28 @@
-// Оголошення змінних та стрілкових функцій.
 /// Користувацька функція для отримання елементу сторінки по id/class/tag.
-const getSelector = arg => document.querySelector(arg);
+const getSelector = arg => document.querySelector(arg),
+	h2 = getSelector('h2');
 
-startGame(8, 8, 50);
+startListener();
 
+function startListener() {
+
+	const btnStart = getSelector('.start__game');
+
+	btnStart.addEventListener('click', (e) => {
+		e.target.nextElementSibling.classList.add('grid__field');
+		// tut style.gridTemplateColumns = `repeat(${myVariable},60px)`;
+		h2.style.opacity = '0';
+		setTimeout(() => h2.innerHTML = '', 400);
+		e.target.remove();
+		startGame(8, 8, 12);
+
+	});
+}
 function startGame(width, height, bombsNumber) {
 	const field = getSelector('.grid__field'), cellsCount = width * height;
 	field.innerHTML = '<button class="btn__cell"></button>'.repeat(cellsCount);
 	const cells = [...field.children];
+	let closedCount = cellsCount;
 
 	const bombs = [...Array(cellsCount).keys()].sort(() => Math.random() - 0.5).slice(0, bombsNumber);
 	field.addEventListener('click', (e) => {
@@ -19,6 +34,10 @@ function startGame(width, height, bombsNumber) {
 		e.target.classList.remove('btn__cell');
 		open(row, column);
 	});
+
+	function isValid(row, column) {
+		return row >= 0 && row < height && column >= 0 && column < width;
+	}
 
 	function getCount(row, column) {
 		let count = 0;
@@ -33,15 +52,56 @@ function startGame(width, height, bombsNumber) {
 	}
 
 	function open(row, column) {
-		const index = row * width + column, cell = cells[index];
+		if (!isValid(row, column)) return;
+		const index = row * width + column;
+		let cell = cells[index];
+		if (cell.disabled === true) return;
+		cell.disabled = true;
 		if (isBomb(row, column)) {
-			cell.className = 'bg__bomb';
-		} else {
-			cell.innerHTML = getCount(row, column);
-			cell.className = 'bg__nobomb';
+			h2.innerHTML = 'You lose!';
+			h2.style.color = 'red';
+			h2.style.opacity = '1';
+			bombs.forEach(el => {
+				cell = cells[el];
+				cell.className = 'bomb__cell';
+			});
+			cells[index].className = 'bomb__redcell';
+			cells.forEach(el => {
+				el.disabled = true;
+				el.classList.remove('btn__cell');
+
+			});
+
+			return;
 		}
+		closedCount--;
+		if (closedCount <= bombsNumber) {
+			h2.innerHTML = 'You won!';
+			h2.style.color = 'lightgreen';
+			h2.style.opacity = '1';
+			return;
+
+		}
+		const count = getCount(row, column);
+		//cell.innerHTML = count;
+		//cell.className = 'bg__nobomb';
+		if (count !== 0) {
+			cell.innerHTML = count;
+			cell.className = 'nobomb__cell';
+			return;
+		} else {
+			cell.classList = 'empty__cell';
+			for (let x = -1; x <= 1; x++) {
+				for (let y = -1; y <= 1; y++) {
+					open(row + y, column + x);
+				}
+			}
+		}
+
+
 	}
 	function isBomb(row, column) {
+		if (!isValid(row, column)) return false;
 		const index = row * width + column;
 		return bombs.includes(index);
 	}
