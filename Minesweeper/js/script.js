a96bab0d082d91c91572326000330728cd81bef3
Edward3635 <Edgard3635@gmail.com>
2022-07-11 21:25:28 +0300

    Minesweeper

--- Minesweeper/js/script.js
+++ Minesweeper/js/script.js
@@ -1,14 +1,14 @@
 /// Користувацька функція для отримання елементу сторінки по id/class/tag.
 const getSelector = arg => document.querySelector(arg), mainTitle = getSelector('.main__title'),
 	h2 = getSelector('h2');
+let isStart = true, sec = 0, min = 0, intervalHandler;
 
 startListener();
 
 function startListener() {
 
 	const btnStart = getSelector('.start__game');
-
-	btnStart.addEventListener('click', (e) => {
+	let btnStartListener = e => {
 		h2.style.opacity = '0';
 		setTimeout(() => {
 			h2.innerHTML = '';
@@ -18,11 +18,20 @@ function startListener() {
 			mainTitle.style.justifyContent = 'space-around';
 		}, 400);
 		e.target.remove();
+		btnStart.removeEventListener('click', btnStartListener);
 
-		startGame(8, 8, 12);
+		setInterval(() => {
+			if (isStart === true) {
+				startGame(8, 8, 12);
+				isStart = false;
+			}
+		}, 300);
 
-	});
+	};
+
+	btnStart.addEventListener('click', btnStartListener);
 }
+
 function startGame(width, height, bombsNumber) {
 	getSelector('.grid__field').style.gridTemplateColumns = `repeat(${width},55px)`;
 	getSelector('.grid__field').style.gridTemplateRows = `repeat(${height},55px)`;
@@ -32,42 +41,52 @@ function startGame(width, height, bombsNumber) {
 	const cells = [...field.children];
 	let closedCount = cellsCount, currentFlagCount = 0;
 	getSelector('.current__flag-count').innerHTML = currentFlagCount;
+	const maxFlagCount = getSelector('.max__flag-count').innerHTML = bombsNumber;
 
 	const bombs = [...Array(cellsCount).keys()].sort(() => Math.random() - 0.5).slice(0, bombsNumber);
-	field.addEventListener('click', (e) => {
+	const fieldClickListener = e => {
 		if (e.target.tagName !== 'BUTTON') {
 			return;
+		}
+		if (e.target.classList.contains('btn__flag')) {
+			return false;
 		}
 
 		const index = cells.indexOf(e.target), column = index % width, row = Math.floor(index / width);
 		e.target.classList.remove('btn__cell');
 		open(row, column);
-	});
-	field.addEventListener('contextmenu', (e) => {
-		if (e.target.tagName !== 'BUTTON') {
-			return;
-		}
-		if (e.target.disabled === true) {
-			return;
-		}
-		if (e.target.classList.contains('btn__flag')) {
-			currentFlagCount--;
-			e.target.classList.replace('btn__flag', 'btn__question');
-			getSelector('.current__flag-count').innerHTML = currentFlagCount;
-
-		} else if (e.target.classList.contains('btn__question')) {
-			e.target.classList.remove('btn__question');
-		} else {
-			if (currentFlagCount < 10) {
-				currentFlagCount++;
-				e.target.classList.add('btn__flag');
+	},
+		fieldContextMenuListener = e => {
+			if (e.target.tagName !== 'BUTTON') {
+				return;
+			}
+			if (e.target.disabled === true) {
+				return;
+			}
+			if (e.target.classList.contains('btn__flag')) {
+				if (currentFlagCount != 0) {
+					currentFlagCount--;
+				}
+				e.target.classList.replace('btn__flag', 'btn__question');
 				getSelector('.current__flag-count').innerHTML = currentFlagCount;
 
-			}
-		}
-		e.preventDefault();
+			} else if (e.target.classList.contains('btn__question')) {
+				e.target.classList.remove('btn__question');
+			} else {
+				if (currentFlagCount < maxFlagCount) {
+					currentFlagCount++;
+					e.target.classList.add('btn__flag');
+					getSelector('.current__flag-count').innerHTML = currentFlagCount;
 
-	});
+				} else {
+					e.target.classList.add('btn__question');
+				}
+			}
+			e.preventDefault();
+
+		};
+	field.addEventListener('click', fieldClickListener);
+	field.addEventListener('contextmenu', fieldContextMenuListener);
 
 	function isValid(row, column) {
 		return row >= 0 && row < height && column >= 0 && column < width;
@@ -90,23 +109,36 @@ function startGame(width, height, bombsNumber) {
 		if (closedCount === cellsCount) {
 			const restartGame = getSelector('.restart__game');
 			restartGame.style.display = 'block';
-			setTimeout(() => restartGame.style.opacity = '1', 400);
+			restartGame.style.visibility = 'unset';
+			setTimeout(() => restartGame.style.opacity = '1', 100);
 			restartGame.addEventListener('click', (e) => {
 				if (!e.target) {
 					return;
 				}
 				h2.style.opacity = '0';
 				restartGame.style.opacity = '0';
-				setTimeout(() => restartGame.style.display = 'none', 400);
-				startGame(8, 8, 12);
+				setTimeout(() => restartGame.style.visibility = 'hidden', 200);
+				field.removeEventListener('click', fieldClickListener);
+				field.removeEventListener('contextmenu', fieldContextMenuListener);
+				clearInterval(intervalHandler);
+				sec = 0, min = 0;
+				getSelector('.timer').style.display = 'none';
+				getSelector('.timer').innerHTML = '00:00';
+				isStart = true;
+				return;
 
 			});
+		}
+		if (closedCount === cellsCount) {
+			getSelector('.timer').style.display = 'block';
+			intervalHandler = setInterval(timer, 1000);
 		}
 		const index = row * width + column;
 		let cell = cells[index];
 		if (cell.disabled === true) return;
 		cell.disabled = true;
 		if (isBomb(row, column)) {
+			clearInterval(intervalHandler);
 			h2.innerHTML = 'You lose!';
 			h2.style.color = 'red';
 			h2.style.display = 'block';
@@ -141,6 +173,7 @@ function startGame(width, height, bombsNumber) {
 					cell.disabled = true;
 					cell.className = 'bomb__cell';
 				});
+				clearInterval(intervalHandler);
 				h2.innerHTML = 'You won!';
 				h2.style.display = 'block';
 				h2.style.color = 'lightgreen';
@@ -163,5 +196,23 @@ function startGame(width, height, bombsNumber) {
 		if (!isValid(row, column)) return false;
 		const index = row * width + column;
 		return bombs.includes(index);
+	}
+	function timer() {
+		// Автоматично додається нуль перед однозначним числом (1-9), по типу 01,02,03... За допомогою padStart()
+		sec++;
+		getSelector('.timer').innerHTML =
+			`${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}`;
+		// Логіка секундоміру: рахування мілісекунд - секунд -хвилини - годин
+		if (sec === 59) {
+			sec = -1, min++;
+			if (min === 60) {
+				min = 59;
+				sec = 59;
+				`${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}`;
+				clearInterval(intervalHandler);
+				return;
+			}
+
+		}
 	}
 }
