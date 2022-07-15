f2b3a79110928e0cd9f8d3f1fd08d06a05e889ca
Edward3635 <Edgard3635@gmail.com>
2022-07-16 00:54:58 +0300

    Lesson 10

--- Lesson_10/Homework/js/script.js
+++ Lesson_10/Homework/js/script.js
@@ -1,50 +1,45 @@
 /// Користувацька функція для отримання елементу сторінки по id/class/tag.
 const getSelector = element => document.querySelector(element),
 	getSelectorAll = element => document.querySelectorAll(element);
-/*
-Реализуйте программу проверки телефона используя DOM Level 2
-Используя JS Создайте поле для ввода телефона и кнопку сохранения
-Пользователь должен ввести номер телефона в формате 000-000-00-00
-Поле того как пользователь нажимает кнопку сохранить проверте правильность ввода номера.
-Если номер правильный сделайте зеленый фон и используя document.location переведите пользователя на страницу 
-https://risovach.ru/upload/2013/03/mem/toni-stark_13447470_big_.jpeg если будет ошибка отобразите её в <div> до input.
-*/
-
-
 
 /* 
-В папке 'banners' лежит HTML код и папка с картинками.
-При запуске программы на экране должна отображаться первая картинка, через 10 секунд вместо нее должна быть показана
-вторая картинка, ещё через 10 секунд - третья, ещё через 10 секунд - четвертая.
-После того, как покажутся все картинки - этот цикл должен начаться заново.
-При запуске программы где-то на экране должна появиться кнопка с надписью `Прекратить`.
-По нажатию на кнопку 'Прекратить' цикл завершается, на экране остается показанной та картинка, которая была
-там при нажатии кнопки. Рядом с кнопкой `Прекратить` должна быть кнопка 'Возобновить показ', при нажатии которой
-цикл продолжается с той картинки, которая в данный момент показана на экране.
-Разметку можно менять, добавлять нужные классы, id, атрибуты, теги.
-*/
+Задание 1
 
+Реализовать слайдер на чистом Javascript.
+
+Технические требования:
+- Создать HTML разметку, содержащую кнопки `Previous`, `Next`, и картинки (6 штук), которые будут пролистываться горизонтально.
+- На странице должна быть видна только одна картинка. Слева от нее будет кнопка `Previous`, справа - `Next`.
+- По нажатию на кнопку `Next` - должна появиться новая картинка, следующая из списка.
+- По нажатию на кнопку `Previous` - должна появиться предыдущая картинка.
+- Слайдер должен быть бесконечным, т.е. если в начале нажать на кнопку `Previous`, то покажется последняя картинка,
+а если нажать на `Next`, когда видимая - последняя картинка, то будет видна первая картинка.
+- Пример работы слайдера можно увидеть(http://kenwheeler.github.io/slick/) (первый пример).
+*/
 const imgWrapper = getSelector('.images-wrapper'), img = getSelectorAll('.image'), prevBtn = getSelector('.slider__prev'),
 	nextBtn = getSelector('.slider__next'), imgGap = parseFloat(getComputedStyle(imgWrapper).gap),
-	btnStart = getSelector('.btn__start'), btnStop = getSelector('.btn__stop');
+	btnStart = getSelector('.btn__start'), btnStop = getSelector('.btn__stop'),
+	[...arrayBubbleButtons] = getSelectorAll('.bubble__button'), bubbleButtons = getSelector('.bubble__buttons');
 
-let size, counter = 1, intervalHandler, isStart = false;
+let size, counter = 1, bubbleCounter = 0, intervalHandler, isStart = false;
 
 window.onload = () => {
 	size = img[0].clientWidth;
 	imgWrapper.style.transform = `translateX(${(-size - imgGap) * counter}px)`;
 };
 
-function nextSlide() {
+function nextSlide(arg) {
 	if (counter >= img.length - 1) return;
 	imgWrapper.style.transition = 'transform 0.8s ease-in-out';
 	counter++;
+	bubbleButtonNextActive(arg);
 	imgWrapper.style.transform = `translate(${(-size - imgGap) * counter}px)`;
 }
-function prevSlide() {
+function prevSlide(arg) {
 	if (counter <= 0) return;
 	imgWrapper.style.transition = 'transform 0.8s ease-in-out';
 	counter--;
+	bubbleButtonPrevActive(arg);
 	imgWrapper.style.transform = `translate(${(-size - imgGap) * counter}px)`;
 }
 
@@ -64,14 +59,14 @@ imgWrapper.addEventListener('transitionend', () => {
 	}
 });
 
-intervalHandler = setInterval(nextSlide, 3000);
+intervalHandler = setInterval(nextSlide, 6000);
 
 nextBtn.onclick = nextSlide;
 prevBtn.onclick = prevSlide;
 
 btnStart.addEventListener('click', () => {
 	if (isStart) {
-		intervalHandler = setInterval(nextSlide, 2000);
+		intervalHandler = setInterval(nextSlide, 6000);
 		isStart = false;
 	}
 });
@@ -79,3 +74,65 @@ btnStop.addEventListener('click', () => {
 	isStart = true;
 	clearInterval(intervalHandler);
 });
+
+function bubbleButtonNextActive(index) {
+	arrayBubbleButtons[bubbleCounter].classList.remove('bubble__active');
+
+	if (typeof index == 'number') {
+		//alert('ok');
+		arrayBubbleButtons[index].classList.add('bubble__active');
+		bubbleCounter = index;
+	} else {
+
+		if (bubbleCounter === arrayBubbleButtons.length - 1) {
+			arrayBubbleButtons[0].classList.add('bubble__active');
+			bubbleCounter = 0;
+
+		} else {
+			arrayBubbleButtons[bubbleCounter + 1].classList.add('bubble__active');
+			bubbleCounter++;
+		}
+	}
+}
+function bubbleButtonPrevActive(index) {
+	arrayBubbleButtons[bubbleCounter].classList.remove('bubble__active');
+	if (typeof index == 'number') {
+		arrayBubbleButtons[index].classList.add('bubble__active');
+		bubbleCounter = index;
+	} else {
+		if (bubbleCounter === 0) {
+			arrayBubbleButtons[arrayBubbleButtons.length - 1].classList.add('bubble__active');
+			bubbleCounter = arrayBubbleButtons.length - 1;
+		} else {
+			arrayBubbleButtons[bubbleCounter - 1].classList.add('bubble__active');
+			bubbleCounter--;
+		}
+	}
+}
+
+bubbleButtons.addEventListener('click', (e) => {
+	if (e.target.classList.contains('bubble__active')) return;
+	arrayBubbleButtons.forEach((el, index) => {
+		if (el === e.target) {
+			counter = index;
+			nextSlide(counter);
+			return;
+
+		}
+	});
+
+
+});
+
+/*
+Задание 2
+Написать реализацию функции, которая позволит создавать объекты типа Hamburger.
+
+Технические требования:
+- Некая сеть фастфудов предлагает два вида гамбургеров: 1) Маленький (50 гривен, 20 калорий) 2) Большой (100 гривен, 40 калорий).
+- Гамбургер должен включать одну дополнительную начинку (обязательно):
+- сыр (+ 10 гривен, + 20 калорий)
+- салат (+ 20 гривен, + 5 калорий)
+- картофель (+ 15 гривен, + 10 калорий)
+- Дополнительно, в гамбургер можно добавить приправу (+ 15 гривен, 0 калорий) и полить майонезом (+ 20 гривен, + 5 калорий).
+*/
