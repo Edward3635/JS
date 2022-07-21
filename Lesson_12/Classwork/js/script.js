917cbaf737235982d16b38d1b12b4dfe37121498
Edward3635 <Edgard3635@gmail.com>
2022-07-21 19:43:59 +0300

    Lesson 12

--- Lesson_12/Classwork/js/script.js
+++ Lesson_12/Classwork/js/script.js
@@ -3,12 +3,18 @@
 const getSelector = arg => document.querySelector(arg);
 
 /*
-Створити форму для реєстрації покупця: 1)Ім'я мінімум 2 літери латиниця; 2)Фамілія мінімум 2 літери кирилиця; 3)Вік число;
-	4)Телефон +38; 5)Індекс число; 6)Та статус за замовчуванням "активний клієнт";
+Створити форму для реєстрації покупця:
+1)Ім'я мінімум 2 літери латиниця;
+2)Фамілія мінімум 2 літери кирилиця;
+3)Вік число;
+4)Телефон +38;
+5)Індекс число;
+6)Та статус за замовчуванням "активний клієнт";
 Клієнт вводить дані про себе і ми перевіряємо їх на правильність.
 Дані які ввів клієнт ми зберігаємо в браузері, та після збереження показуємо дані на сторінці у вигляді таблиці,
 де мають бути показані тільки діючі клієнти.
 */
+
 const mainForm = getSelector('.main__form');
 mainForm.addEventListener('change', (e) => {
 	Validate(e.target);
@@ -18,15 +24,63 @@ mainForm.addEventListener('change', (e) => {
 function Validate(target) {
 	switch (target.id) {
 		case 'name': return /^[A-z]{2,}$/i.test(target.value);
-		case 'surname': return /^[А-я]{2,}$/i.test(target.value);
+		case 'surname': return /^[А-я/і/ї/є]{2,}$/i.test(target.value);
 		case 'age': return /^\d{1,2}$/i.test(target.value);
 		case 'tel': return /^\+380\d{9}$/i.test(target.value);
 		case 'index': return /^\d{5}$/i.test(target.value);
+		default:
+			throw new Error('Невірний виклик регулярного виразу');
 	}
 }
+localStorage.user = JSON.stringify([]);
 const btnSave = getSelector('input[type="button"]');
 btnSave.addEventListener('click', () => {
-	// if (!arr.includes(false)) {
-	// 	console.log('Ok!');
-	// }
+	const [...arrInputs] = document.querySelectorAll('.main__form > input:not(input[type="button"])');
+	const res = arrInputs.map(el => {
+		return Validate(el);
+	});
+	if (!res.includes(false)) {
+		let userData = JSON.parse(localStorage.user);
+		userData.push(
+			new User(
+				...arrInputs.map(el => {
+					return el.value;
+				})));
+		localStorage.user = JSON.stringify(userData);
+		arrInputs.forEach(el => el.value = '');
+		if (!getSelector('.grid__data')) {
+			createGrid();
+		}
+		createUserList();
+		localStorage.user = JSON.stringify([]);
+	}
 });
+class User {
+	constructor(name, surname, age, phone, index) {
+		this.name = name;
+		this.surname = surname;
+		this.age = age;
+		this.phone = phone;
+		this.index = index;
+		this.name = name;
+		this.status = true;
+	}
+}
+
+function createUserList() {
+	const currentUsers = JSON.parse(localStorage.user), gridData = getSelector('.grid__data');
+	currentUsers.forEach(el => {
+		gridData.insertAdjacentHTML('beforeend', `<div>${el.name}</div><div>${el.surname}</div><div>${el.age}</div>` +
+			`<div>${el.phone}</div><div>${el.index}</div>`);
+		if (el.status) {
+			gridData.insertAdjacentHTML('beforeend', '<div><input type ="checkbox" checked></div>');
+		} else {
+			gridData.insertAdjacentHTML('beforeend', '<div><input type ="checkbox"></div>');
+		}
+	});
+}
+
+function createGrid() {
+	getSelector('.main__form').insertAdjacentHTML('afterend', '<div class="grid__data"><div>Name</div><div>Surname</div>' +
+		'<div>Age</div><div>Phone</div><div>Index</div><div>Status</div></div>');
+}
--- Lesson_12/Homework/js/script.js
+++ Lesson_12/Homework/js/script.js
@@ -1,3 +1,38 @@
-// Оголошення змінних та стрілкових функцій.
-/// Користувацька функція для отримання елементу сторінки по id/class/tag.
-const getSelector = arg => document.querySelector(arg);
+/*
+Завдання:
+Реалізувати функцію підсвічування клавіш
+Технічні вимоги:
+- Кожна кнопка містить назву клавіші на клавіатурі
+- Після натискання названих клавіш - та кнопка , на якій написана ця літера, повинна фарбуватись у синій колір.
+При цьому якщо якась інша  літера вже раніше була пофарбована в синій колір - вона стає чорною. Наприклад натисканням Enter
+пешра кнопка забарвлюється в синій колір. Далі, користувач натискає 'S', і кнопка забарвлюється в синій колір, а кнопка 'Enter'
+знову стає чорною.
+*/
+
+let keyInMemory;
+function changeBtn(arr, key) {
+
+	if (keyInMemory) {
+		keyInMemory.style.background = 'black';
+	}
+	arr.forEach(el => {
+		if (el.textContent === key) {
+			el.style.background = 'blue';
+			keyInMemory = el;
+		}
+	});
+}
+
+window.addEventListener('keyup', (e) => {
+	const [...buttons] = document.querySelectorAll('.btn'),
+		[...btnNames] = buttons.map(el => {
+			return el.textContent;
+		});
+	let key;
+	if (e.code.slice(0, 3) === 'Key') {
+		key = e.code.slice(3);
+	} else key = e.code;
+	if (btnNames.includes(key)) {
+		changeBtn(buttons, key);
+	}
+});
