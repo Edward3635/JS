69a76cd35c07b3894110720442566eefe2062137
Edward3635 <Edgard3635@gmail.com>
2022-08-19 21:26:38 +0300

    Lesson 16

--- lesson_16/src/components/currencyTable.jsx
+++ lesson_16/src/components/currencyTable.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 function Table(props) {
 	return (
 		<div className='grid__table'>
-			{props.data.map((el, index) => {
-				return <div key={index}>{el.txt}</div>;
-			})}
+			<div className='currency__number'>
+				{props.data.map((el, index) => {
+					return <div key={index}>{index + 1}</div>;
+				})}
+			</div>
+			<div className='currency__name'>
+				{props.data.map((el, index) => {
+					return <div key={index}>{el.txt}</div>;
+				})}
+			</div>
+			<div className='currency__price'>
+				{props.data.map((el, index) => {
+					return <div key={index}>{el.rate}</div>;
+				})}
+			</div>
 		</div>
 	);
 }
