14a9f79c0fdb485c3104416565618312139a1617
Edward3635 <Edgard3635@gmail.com>
2022-08-21 14:16:56 +0300

    pet-project

--- pet-project/src/index.js
+++ pet-project/src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/App';
 import './style/css/style.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const App = function () {
-	return (
-		<div>
-			Pet-project!
-		</div>
-	)
-};
-root.render(<App />);
+root.render(<App/>);
