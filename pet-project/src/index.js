dc69d01b3938f12c8b54f9727cb26e739526231d
Edward3635 <Edgard3635@gmail.com>
2022-08-27 15:54:24 +0300

    react proj removed

--- pet-project/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
-import './style/css/style.css';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(<App/>);
